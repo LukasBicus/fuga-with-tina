6e6c84de76e8228983fbe2616ad3f48f41468589
lukasbicus <ing.lukas.bicus@gmail.com>
2023-11-24 08:24:59 +0100

    Play with content modeling

--- content/post/lukas-page.md
+++ content/post/lukas-page.md
@@ -4,7 +4,11 @@ tags:
   - Person
   - Contractor
   - Developer
+authors:
+  - name: Lukas Bicus
+    email: l.b@g.c
 ---
+
 
 
 Hi mi name is Lukas. I'm software developer.
--- tina/collections/post.js
+++ tina/collections/post.js
@@ -1,30 +1,59 @@
 /**
- * @type {import('tinacms').Collection}
+ * @type {import("tinacms").Collection}
  */
 export default {
   label: "Blog Posts",
   name: "post",
   path: "content/post",
+  defaultItem: () => {
+    return {
+      title: 'A default title',
+      body: 'What a nice body'
+    }
+  },
   fields: [
     {
       type: "string",
       label: "Title",
       name: "title",
     },
-    {type: "string", label: "Tags", name: "tags", list: true},
+    {
+      type: "string", label: "Tags", name: "tags", list: true,
+      defaultItem: () => {
+        return `A tag`;
+      }
+    },
     {
       type: "string",
       label: "Blog Post Body",
       name: "body",
       isBody: true,
       ui: {
-        component: "textarea",
-      },
+        component: "textarea"
+      }
     },
+    {
+      type: "object",
+      label: "Authors",
+      name: "authors",
+      list: true,
+      fields: [
+        {
+          type: "string",
+          label: "Name",
+          name: "name",
+        },
+        {
+          type: "string",
+          label: "Email",
+          name: "email"
+        }
+      ]
+    }
   ],
   ui: {
     router: ({ document }) => {
       return `/posts/${document._sys.filename}`;
-    },
-  },
+    }
+  }
 };
